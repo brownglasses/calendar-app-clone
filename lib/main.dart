6c614b84f3f777c54d236511f53ac227638db694
brownglasses <lhj1583@naver.com>
2023-12-16 13:42:58 +0900

    feat/calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:project_c/screen/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: TableCalendar(
-          firstDay: DateTime(1900, 1, 1),
-          focusedDay: DateTime.now(),
-          lastDay: DateTime(2999, 12, 31),
-          selectedDayPredicate: (DateTime day) {
-            final now = DateTime.now();
-            return DateTime(day.year, day.month, day.day)
-                .isAtSameMomentAs(DateTime(now.year, now.month, now.day));
-          },
-          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {},
-          onPageChanged: (DateTime focusedDay) {},
-          rangeSelectionMode: RangeSelectionMode.toggledOff,
-          onRangeSelected:
-              (DateTime? start, DateTime? end, DateTime focusedDay) {},
-        ),
-      ),
-    ),
+    const MaterialApp(home: HomeScreen()),
   );
 }
