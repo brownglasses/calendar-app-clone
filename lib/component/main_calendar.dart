8129613cbc71c32979837a67cd3aa07c8908d1dd
brownglasses <lhj1583@naver.com>
2023-12-18 23:07:46 +0900

    korea_keyboard

--- lib/component/main_calendar.dart
+++ lib/component/main_calendar.dart
@@ -10,6 +10,7 @@ class MainCalendar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
+      locale: 'ko_kr',
       onDaySelected: onDaySelected,
       selectedDayPredicate: (date) =>
           date.year == selectedDate.year &&
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:project_c/screen/home_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
   runApp(
     const MaterialApp(home: HomeScreen()),
   );
